2a7c842a333233cfb56abad309760174653d6fa4
RomanRiyanov <yezgo123@gmail.com>
2023-09-27 13:13:09 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue_project
+
+## Created with the goal of practicing creating applications using Vue and Nuxt
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- <Tutorial /> -->
+  <h1>HELLO RAAAAAn</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
